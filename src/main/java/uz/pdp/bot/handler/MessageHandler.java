d004b30d3bafd29ada0dc8279a197db299533b48
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2024-05-09 12:14:00 +0500

    remove temp methods

--- src/main/java/uz/pdp/bot/handler/MessageHandler.java
+++ src/main/java/uz/pdp/bot/handler/MessageHandler.java
@@ -4,8 +4,6 @@ import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.request.SendPoll;
-import uz.pdp.backend.model.question.Question;
 import uz.pdp.backend.model.variation.Variation;
 
 import java.util.Objects;
@@ -27,24 +25,13 @@ public class MessageHandler extends BaseHandler {
 
             SendMessage sendMessage = new SendMessage(chat.id(), "Hello! ");
             bot.execute(sendMessage);
-
-            Question question = new Question(null, "Hello? ");
-            Variation variation1 = new Variation(question.getId(), "Hello");
-            Variation variation2 = new Variation(question.getId(), "Hi");
-            Variation variation3 = new Variation(question.getId(), "Ola");
-            Variation variation4 = new Variation(question.getId(), "Gracia");
-
-
-            SendPoll sendPoll = new SendPoll(chat.id(), question.getText(), getOptions(variation1, variation2, variation3, variation4));
-
-            bot.execute(sendPoll);
         }
     }
 
     private String[] getOptions(Variation... variations) {
         String[] options = new String[variations.length];
         for (int i = 0; i < variations.length; i++) {
-            options[i] = variations[i].getText();
+            options[i] = variations[i].getAnswer();
         }
 
         return options;
